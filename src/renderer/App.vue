80d5282a212c3fac157d8e6b4b684111a56bc196
irraky <irraky@caribou>
2018-07-20 18:28:03 +0200

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'webview'
+  }
+</script>
+
+<style>
+  @import './style/css/reset';
+  @import './style/css/kiosk';
+  /* CSS */
+</style>
